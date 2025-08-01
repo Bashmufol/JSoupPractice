c3106ef01629dbd3e67e27cabd3437eb04ed843c
Bashir Muhammed <bashmufol@gmail.com>
2025-08-01 11:25:34 +0100

    first commit

---
+++ src/main/java/com/bash/jsouppractice/JSoupPracticeApplication.java
@@ -0,0 +1,27 @@
+package com.bash.jsouppractice;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+
+@SpringBootApplication
+public class JSoupPracticeApplication {
+
+    public static void main(String[] args) {
+
+        String url = "https://books.toscrape.com/";
+        try{
+            Document doc = Jsoup.connect(url).get();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
+        SpringApplication.run(JSoupPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bash/jsouppractice/JSoupPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bash.jsouppractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JSoupPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
