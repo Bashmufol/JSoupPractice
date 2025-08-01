bfa5e2d1082edd5755b647d1134b69a54a2254a6
Bashir Muhammed <bashmufol@gmail.com>
2025-08-01 11:38:21 +0100

    updated

--- src/main/java/com/bash/jsouppractice/JSoupPracticeApplication.java
+++ src/main/java/com/bash/jsouppractice/JSoupPracticeApplication.java
@@ -2,6 +2,7 @@ package com.bash.jsouppractice;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -15,7 +16,7 @@ public class JSoupPracticeApplication {
         String url = "https://books.toscrape.com/";
         try{
             Document doc = Jsoup.connect(url).get();
-            books = doc.select("")
+            Elements books = doc.select("product_pod");
         } catch (IOException e) {
             e.printStackTrace();
         }
